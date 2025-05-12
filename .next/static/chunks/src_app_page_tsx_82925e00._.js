506531ae7daec11bd2d4a7c3d0645824522da27c
Rémy <remy.berjonneau@gmail.com>
2025-05-12 09:15:59 +0200

    missing style

---
+++ .next/static/chunks/src_app_page_tsx_82925e00._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_de989b73._.js",
+  "static/chunks/src_styles_Home_module_scss_module_e1af5674.css"
+],
+    source: "dynamic"
+});
